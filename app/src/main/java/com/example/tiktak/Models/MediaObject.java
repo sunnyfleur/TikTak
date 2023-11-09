c4847b47bb6d2987d8eaca460c1cd26478c21873
Tran Hoang Kiet <116096297+sunnyfleur@users.noreply.github.com>
2023-11-09 19:39:22 +0700

    7:38 09/11/2023
    Hoàn thành sửa lỗi Merge
    Thay đổi android manifest

--- app/src/main/java/com/example/tiktak/Models/MediaObject.java
+++ app/src/main/java/com/example/tiktak/Models/MediaObject.java
@@ -59,63 +59,36 @@ public class MediaObject {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
 
     public String getUser_id() {
         return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
 
     public String getPost_categories() {
         return post_categories;
     }
 
-    public void setPost_categories(String post_categories) {
-        this.post_categories = post_categories;
-    }
-
     public String getPost_id() {
         return post_id;
-    }
-
-    public void setPost_id(String post_id) {
-        this.post_id = post_id;
     }
 
     public String getView() {
         return view;
     }
 
-    public void setView(String view) {
-        this.view = view;
-    }
 
     public String getUser_name() {
         return user_name;
     }
 
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
 
     public String getMedia_url() {
         return media_url;
     }
 
-    public void setMedia_url(String media_url) {
-        this.media_url = media_url;
-    }
 
     public String getThumbnail() {
         return thumbnail;
     }
 
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
 }
